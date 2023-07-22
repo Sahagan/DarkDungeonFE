5a8bf37dd1874eccb1fbc91860056b3ae1642985
aob <sahan457@cubes.postbox.in.th>
2023-07-22 21:05:24 +0700

    add Name Confirmation Message

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostListener, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { getUrl, playButtonSound, playInputPlayerName, playrainEffect, setVolumeBackgroundMusic, setVolumeEffect, stopAllMusic} from '../../services/common/utility.service';
+import { getUrl, playButtonSound, playInputPlayerName, playrainEffect, setVolumeBackgroundMusic, setVolumeEffect, stopAllMusic } from '../../services/common/utility.service';
 import { RequestService } from 'src/app/services/common/request.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
@@ -20,18 +20,19 @@ export class StartComponent {
   constructor(
     private router: Router,
     private RequestService: RequestService
-  ) {}
+  ) { }
   public state = 'hidden';
-  playerName : any;
-  EffectVolume : any;
+  playerName: any;
+  EffectVolume: any;
   MusicVolume: any;
   //Input
   showNameInput: boolean = true;
+  NameConfirm: boolean = false;
   async ngOnInit(): Promise<void> {
     playInputPlayerName();
-    await setTimeout(()=>{
+    await setTimeout(() => {
       this.state = 'visible';
-    },1000);
+    }, 1000);
   };
 
   onEffectVolumeChange(): void {
@@ -49,31 +50,42 @@ export class StartComponent {
     this.router.navigate(['/home']);
   };
 
-  async onEnterClicked(){
+  async onEnterClicked() {
     playButtonSound();
-    let response:any;
-    let url:any;
-    try{
-      if(!this.playerName){
+    try {
+      if (!this.playerName) {
         throw `Player name cannot be empty!`
-      }else if(this.playerName){
-        if(/^[A-Za-z0-9]{1,10}$/.test(this.playerName) === false){
+      } else if (this.playerName) {
+        if (/^[A-Za-z0-9]{1,10}$/.test(this.playerName) === false) {
           throw `Invalid PlayerName \n please enter only Character[English] or Number without Spacebar \n The name should be less than 10 characters`
         }
       };
-      url = await getUrl('username');
-      response = await this.RequestService.postData(url,{"playerName" : this.playerName});
-      if(response.resultCode === '20000'){
-        this.showNameInput = false;
-        url = await getUrl('map');
-        response = await this.RequestService.getData(url);
-        //response = 
-      }else{
-        throw `Service Unavailable`
-      }
-    }catch(error){
+      this.showNameInput = false;
+      this.NameConfirm = true;
+    } catch (error) {
       alert(error);
     }
   };
+  async onConfirmClicked() {
+    playButtonSound();
+    let response: any;
+    let url: any;
+    url = await getUrl('username');
+    response = await this.RequestService.postData(url, { "playerName": this.playerName });
+    if (response.resultCode === '20000') {
+      this.showNameInput = false;
+      url = await getUrl('map');
+      response = await this.RequestService.getData(url);
+      //response = 
+    } else {
+      throw `Service Unavailable`
+    }
+  }
+
+  async onNoClicked() {
+    playButtonSound();
+    this.NameConfirm = false;
+    this.showNameInput = true;
+  }
 
 }
