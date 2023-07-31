3ed51ab573df9138ccd12ab52831084c77652d94
aob <sahan457@cubes.postbox.in.th>
2023-07-31 23:02:16 +0700

    hide player Input

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -78,6 +78,7 @@ export class StartComponent {
   };
   async onConfirmClicked() {
     playButtonSound();
+    this.NameConfirm = false;
     this.isConfirmClicked = true;
     let response: any;
     let url: any;
